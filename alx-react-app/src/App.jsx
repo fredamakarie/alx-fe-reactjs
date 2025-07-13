ccd468f3525a14d0d67956ab28c3a97e3c0fd1b9
Omonye Omonlumhen <omonyeomonlumhen@gmail.com>
2025-07-13 18:57:04 +0100

    created first react app

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import WelcomeMessage from './components/WelcomeMessage'
+import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
+import Footer from './components/Footer'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <Header />
+      <WelcomeMessage />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
+    </>
+  )
+}
+
+export default App
