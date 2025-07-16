780cc1d36186167d86b3fa28a89a9db465dafaf4
Omonye Omonlumhen <omonyeomonlumhen@gmail.com>
2025-07-16 17:07:40 +0100

    final file update

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,13 @@
-
-import UserDetails from "./UserDetails"
+import { useContext } from "react";
+import userContext from "./UserContext";
 
 function UserProfile() {
+  const userData = useContext(userContext)
   return (
-    <UserDetails />
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
   );
 }
 
