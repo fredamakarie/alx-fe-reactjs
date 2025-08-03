af9e71f83b17e4ee8b75c36d8ecb8ae71908a572
Omonye Omonlumhen <omonyeomonlumhen@gmail.com>
2025-08-03 19:47:49 +0100

    api exercises

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes, Router } from 'react-router-dom'
+import Search from './components/Search'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1 style={{color:'grey', backgroundColor: 'whitesmoke', padding: '2rem'}}><span>GitHub</span> User Search</h1>
+     
+        <Routes>
+          <Route/>
+        </Routes>
+     <Search/>
+
+    </>
+  )
+}
+
+export default App
