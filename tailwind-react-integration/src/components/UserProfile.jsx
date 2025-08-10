6b8ab297e44370bd9165fd6f3750745b39355a61
Omonye Omonlumhen <omonyeomonlumhen@gmail.com>
2025-08-10 22:13:26 +0100

    edits to fix alx checker

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-8 max-w-sm mx-auto,my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm hover:shadow-xl">
-      <img src="./src/assets" alt="User"  className="rounded-full mx-auto md:h-36,w-36 sm:h-24,w-24 hover:scale-110 transition-transform duration-300 ease-in-out"/>
+      <img src="./src/assets" alt="User"  className="rounded-full mx-auto md:h-36 md:w-36 sm:h-24 s:w-24 hover:scale-110 transition-transform duration-300 ease-in-out"/>
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">John Doe</h1>
       <p className="text-gray-600 sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
