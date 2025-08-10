4c2a652f67d4274581d74050aaf3c74fcc6ca390
Omonye Omonlumhen <omonyeomonlumhen@gmail.com>
2025-08-10 17:00:10 +0100

    tailwind css styling exercises

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes, Router } from 'react-router-dom'
+import './App.css'
+import UserProfile from './components/UserProfile'
+
+function App() {
+  
+
+  return (
+    <>
+     
+     
+        <Routes>
+          <Route/>
+        </Routes>
+     
+      <UserProfile/>
+    </>
+  )
+}
+
+export default App
